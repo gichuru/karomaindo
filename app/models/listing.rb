0e5b5be0b8baaefefb5aecb1b31bac21acdebf41
gichuru <gichuru@gmail.com>
2017-01-27 17:40:31 +0300

    Images paperclip aws

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,11 @@
 class Listing < ApplicationRecord
-	has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
+	#has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
+	has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  },
+  	default_url: "default.jpg"
+  	#Validates the attached image is a images/jpeg or images/png
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
